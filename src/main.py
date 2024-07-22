57487164d3035e2b6406cc02c3cd7e360c4e0294
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-07-22 12:24:48 -0400

    cleaned up repo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from lc3 import LC
+import jinja2
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Drink more coffee Right now'
+@app.route("/")
+def landing():
+    return render_template('index.html')
+
 
 app.run(host = '0.0.0.0', port = 80)
+
+
+    
